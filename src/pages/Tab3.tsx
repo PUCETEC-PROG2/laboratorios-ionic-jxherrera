fa45658873b4a018208e9d2c950599cb73671063
Usuario invitado <Guest@iMac-de-PUCE-A10713.local>
2026-06-17 15:51:29 -0500

    lab-ionic

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonCardHeader, IonPage, IonTitle, IonToolbar, IonHeader } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,32 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className='card-container'>
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/210150544?s=400&v=4" alt="" />
+          <IonCardHeader>
+            <IonCardTitle>Josue Herrera</IonCardTitle>
+            <IonCardSubtitle>jxherrera</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+              <p>
+                todo rojo
+              </p>
+          </IonCardContent>
+          </IonCard>
+        </div>
+
+
       </IonContent>
     </IonPage>
   );
